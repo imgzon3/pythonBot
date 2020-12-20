449ea43346368380cc9e6df7ddbfa0a19b20e50f
imgzon3 <imgzon3@naver.com>
2020-12-20 23:58:47 +0900

    접두사 변경

--- pythonBot/bot_info.py
+++ pythonBot/bot_info.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=';')  # 명령어 접두사는 ;으로 지정
+bot = commands.Bot(command_prefix='//')  # 명령어 접두사는 ;으로 지정
 bot.remove_command('help')  # 기본 명령어 help 미리 제거
 
 # 토큰 값 가져오기
