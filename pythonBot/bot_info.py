20424b63e555c38cc579c46cde95a3765aa66ab5
imgzon3 <imgzon3@naver.com>
2020-12-20 23:09:16 +0900

    토큰 비공개

--- pythonBot/bot_info.py
+++ pythonBot/bot_info.py
@@ -4,4 +4,4 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix=';')  # 명령어 접두사는 ;으로 지정
 bot.remove_command('help')  # 기본 명령어 help 미리 제거
 
-token = 'NjU5MDAzMDM1NTA2MTgwMTA3.XgH-Zw.DE13zoP060E2Ayx3jOcd_kNFhEA'
+token = ''
